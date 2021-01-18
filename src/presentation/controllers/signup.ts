60e524ae5f388d7a26ccf9677e3c25f5b7476c66
Victor Piccoli <victorpicc@gmail.com>
2021-01-18 20:12:20 -0200

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,6 +4,7 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 import Controller from '../protocols/controller';
 import EmailValidator from '../protocols/email-validator';
 import InvalidParamError from '../errors/invalid-param-error';
+import ServerError from '../errors/server-error';
 
 /* eslint-disable class-methods-use-this */
 export default class SignUpController implements Controller {
@@ -14,16 +15,20 @@ export default class SignUpController implements Controller {
   }
 
   handle(httpRequest : HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
-    for (let i = 0; i < requiredFields.length; i += 1) {
-      if (!httpRequest.body[requiredFields[i]]) {
-        return badRequest(new MissingParamError(requiredFields[i]));
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
+      for (let i = 0; i < requiredFields.length; i += 1) {
+        if (!httpRequest.body[requiredFields[i]]) {
+          return badRequest(new MissingParamError(requiredFields[i]));
+        }
       }
+      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!emailIsValid) {
+        return badRequest(new InvalidParamError('email'));
+      }
+      return internalError(new Error('Internal server error'));
+    } catch (error) {
+      return internalError(new ServerError());
     }
-    const emailIsValid = this.emailValidator.isValid(httpRequest.body.email);
-    if (!emailIsValid) {
-      return badRequest(new InvalidParamError('email'));
-    }
-    return internalError(new Error('Internal server error'));
   }
 }

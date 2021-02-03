58cc030fc7f66424b3ca3040f7590373d349e998
Victor Piccoli <victorpicc@gmail.com>
2021-02-03 00:31:17 -0200

    feat: ensure requests return json as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -1,9 +1,9 @@
 import request from 'supertest';
 import app from '../config/app';
 
-describe('CIRS Middleware', () => {
+describe('CORS Middleware', () => {
   test('should enable CORS', async () => {
-    app.post('/test_cors', (req, res) => {
+    app.get('/test_cors', (req, res) => {
       res.send();
     });
     await request(app)

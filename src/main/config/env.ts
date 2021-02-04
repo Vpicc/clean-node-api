2f6fdd2a16ecdf5d26a9f1e6226568ef143a1206
Victor Piccoli <victorpicc@gmail.com>
2021-02-03 22:21:17 -0200

    feat: add ExpressRouteAdapter

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 const env = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
 };
 

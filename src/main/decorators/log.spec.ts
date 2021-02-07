47af1499c4dbba7582afdddbb9dd8b6a667730cc
Victor Piccoli <victorpicc@gmail.com>
2021-02-07 20:02:07 -0200

    test: ensure LogControllerDecorator returns the same result as controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -43,4 +43,23 @@ describe('LogController Decorator', () => {
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });
+
+  test('should return the same result as controller', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const result = await sut.handle(httpRequest);
+    expect(result).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Victor',
+      },
+    });
+  });
 });

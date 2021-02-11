96c87fc968dd40e1b91b56347b1b02aae19f24c8
Victor Piccoli <victorpicc@gmail.com>
2021-02-10 22:26:14 -0200

    test: ensure CompareFieldsValidation functions as expected

---
+++ src/presentation/helpers/validators/compare-fields-validatio.spec.ts
@@ -0,0 +1,20 @@
+import { InvalidParamError } from '../../errors';
+import CompareFieldsValidation from './compare-fields-validation';
+
+interface SutTypes {
+  sut: CompareFieldsValidation,
+}
+
+const makeSut = (): SutTypes => ({ sut: new CompareFieldsValidation('original', 'field') });
+describe('CompareFields Validation', () => {
+  test('should return MissingParamError if validation fails', () => {
+    const { sut } = makeSut();
+    const response = sut.validate({ original: 'any_name', field: 'wrong_name' });
+    expect(response).toEqual(new InvalidParamError('field'));
+  });
+  test('should return null if validation succeeds', () => {
+    const { sut } = makeSut();
+    const response = sut.validate({ original: 'any_name', field: 'any_name' });
+    expect(response).toEqual(null);
+  });
+});
--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,14 +1,19 @@
 import { MissingParamError } from '../../errors';
 import RequiredFieldValidation from './required-field-validation';
 
+interface SutTypes {
+  sut: RequiredFieldValidation,
+}
+
+const makeSut = (): SutTypes => ({ sut: new RequiredFieldValidation('field') });
 describe('RequiredField Validation', () => {
   test('should return MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('field');
+    const { sut } = makeSut();
     const response = sut.validate({ name: 'any_name' });
     expect(response).toEqual(new MissingParamError('field'));
   });
-  test('should return MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('field');
+  test('should return null if validation succeeds', () => {
+    const { sut } = makeSut();
     const response = sut.validate({ field: 'any' });
     expect(response).toEqual(null);
   });

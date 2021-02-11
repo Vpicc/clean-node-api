3c209f14008b43f175e5259997e2b1c6afd597b2
Victor Piccoli <victorpicc@gmail.com>
2021-02-10 22:18:21 -0200

    test: ensure RequiredFieldValidation return null if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,9 @@ describe('RequiredField Validation', () => {
     const response = sut.validate({ name: 'any_name' });
     expect(response).toEqual(new MissingParamError('field'));
   });
+  test('should return MissingParamError if validation fails', () => {
+    const sut = new RequiredFieldValidation('field');
+    const response = sut.validate({ field: 'any' });
+    expect(response).toEqual(null);
+  });
 });

3d15b7ed935d4de7c82ea8d748850ce32f101c06
Victor Piccoli <victorpicc@gmail.com>
2021-02-16 13:17:22 -0200

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,6 +2,7 @@ import Authentication, { AuthenticationModel } from '../../../domain/usecases/au
 import { HashComparer } from '../../protocols/criptography/hash-comparer';
 import TokenGenerator from '../../protocols/criptography/token-generator';
 import LoadAccountByEmailRepository from '../../protocols/db/load-account-by-email-repository';
+import UpdateAccessTokenRepository from '../../protocols/db/update-access-token-repository';
 
 export default class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository;
@@ -10,14 +11,18 @@ export default class DbAuthentication implements Authentication {
 
   private readonly tokenGenerator;
 
+  private readonly updateAccessTokenRepository;
+
   constructor(
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
     tokenGenerator: TokenGenerator,
+    updateAccessTokenRepository: UpdateAccessTokenRepository,
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
     this.hashComparer = hashComparer;
     this.tokenGenerator = tokenGenerator;
+    this.updateAccessTokenRepository = updateAccessTokenRepository;
   }
 
   async auth(authentication: AuthenticationModel): Promise<string | null> {
@@ -34,6 +39,8 @@ export default class DbAuthentication implements Authentication {
     }
 
     const token = await this.tokenGenerator.generate(account.id);
+
+    await this.updateAccessTokenRepository.update(account.id, token);
 
     return token;
   }

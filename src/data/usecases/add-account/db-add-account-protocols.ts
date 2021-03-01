91a5386f7a0cc04685cc1a69d3c3aa72cc261210
Victor Piccoli <victorpicc@gmail.com>
2021-03-01 17:28:13 -0300

    feat: ensure DbAddAccount calls loadAccountByEmailRepository with correct values

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -2,7 +2,13 @@ import { AccountModel } from '../../../domain/models/account';
 import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account';
 import Hasher from '../../protocols/criptography/hasher';
 import AddAccountRepository from '../../protocols/db/account/add-account-repository';
+import LoadAccountByEmailRepository from '../../protocols/db/account/load-account-by-email-repository';
 
 export {
-  AccountModel, AddAccount, AddAccountModel, Hasher, AddAccountRepository,
+  AccountModel,
+  AddAccount,
+  AddAccountModel,
+  Hasher,
+  AddAccountRepository,
+  LoadAccountByEmailRepository,
 };
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,14 +1,21 @@
 import {
-  AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Hasher,
+  AccountModel,
+  AddAccount,
+  AddAccountModel,
+  AddAccountRepository,
+  Hasher,
+  LoadAccountByEmailRepository,
 } from './db-add-account-protocols';
 
 export default class DbAddAccount implements AddAccount {
   constructor(
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
   ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
+    this.loadAccountByEmailRepository.loadByEmail(accountData.email);
     const hashedPassword = await this.hasher.hash(accountData.password);
     const account = await this.addAccountRepository.add({
       ...accountData,

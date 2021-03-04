99c8ac6edfb8e2655da7612a69554c60616919ce
Victor Piccoli <victorpicc@gmail.com>
2021-03-04 08:49:21 -0300

    test: ensure AddSurveyController calls Validation with correct values

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Validation } from '../../../protocols';
+import AddSurveyController from './add-survey-controller';
+import { Controller, HttpRequest } from './add-survey-controller-protocols';
+
+const makeFakeSurvey = () => (
+  {
+    question: 'any question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer',
+    }],
+  }
+);
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: makeFakeSurvey(),
+});
+
+interface SutTypes {
+  sut: Controller,
+  validationStub: Validation,
+}
+
+const makeValidationStub = () => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error | null {
+      return null;
+    }
+  }
+  return new ValidationStub();
+};
+
+const makeSut = () : SutTypes => {
+  const validationStub = makeValidationStub();
+  const sut = new AddSurveyController(validationStub);
+  return { sut, validationStub };
+};
+
+describe('AddSurvey Controller', () => {
+  test('should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut();
+    const validateSpy = jest.spyOn(validationStub, 'validate');
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+    expect(validateSpy).toHaveBeenCalledWith(makeFakeSurvey());
+  });
+});

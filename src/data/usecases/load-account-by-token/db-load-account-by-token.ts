9f7e1262af401c14f02e7d357dd9375fc02d4c03
Victor Piccoli <victorpicc@gmail.com>
2021-03-18 16:14:55 -0300

    fix: ensure loadByToken is called with accessToken

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -11,10 +11,11 @@ export default class DbLoadAccountByToken implements LoadAccountByToken {
 
   async load(accessToken: string, role?: string): Promise<AccountModel | null> {
     const token = await this.decrypter.decrypt(accessToken);
+
     if (!token) {
       return null;
     }
-    const account = await this.loadAccountByTokenRepository.loadByToken(token, role);
+    const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role);
     return account;
   }
 }

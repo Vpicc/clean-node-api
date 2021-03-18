663a5859c70a15805f70b7bdb8e1329899527a19
Victor Piccoli <victorpicc@gmail.com>
2021-03-18 16:16:06 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-underscore-dangle */
 import { Collection } from 'mongodb';
 import request from 'supertest';
+import { sign } from 'jsonwebtoken';
 import MongoHelper from '../../infra/db/mongodb/helpers/mongo-helper';
+import env from '../config/env';
 import app from '../config/app';
 import { AddSurveyModel } from '../../domain/usecases/add-survey';
 
@@ -15,6 +18,7 @@ const makeFakeSurvey = () : AddSurveyModel => ({
 });
 
 let surveyCollection: Collection;
+let accountCollection: Collection;
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -28,6 +32,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
   });
   describe('POST /surveys', () => {
     test('should return 403 on add survey without accessToken', async () => {
@@ -35,6 +41,31 @@ describe('Survey Routes', () => {
         .post('/api/surveys')
         .send(makeFakeSurvey())
         .expect(403);
+    });
+
+    test('should return 204 on add survey with valid accessToken', async () => {
+      const account = {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'valid_password',
+        role: 'admin',
+      };
+      const res = await accountCollection.insertOne(account);
+      const id = res.ops[0]._id;
+      const accessToken = sign({ id }, env.jwtSecret);
+      await accountCollection.updateOne({
+        _id: id,
+      }, {
+        $set: {
+          accessToken,
+        },
+      });
+      console.log(await accountCollection.findOne({ _id: id }));
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send(makeFakeSurvey())
+        .expect(204);
     });
   });
 });
